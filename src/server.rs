8c415879103f5ff261cc841c2151210825916d57
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2021-01-09 07:19:20 +0530

    update email + type conversions

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::io::Read;
 use std::net::TcpListener;
 
@@ -20,6 +23,12 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(err) => {
+                                    println!("Failed to parse a request: {}", err)
+                                }
+                            }
                         }
 
                         Err(err) => println!("Error: {}", err),
